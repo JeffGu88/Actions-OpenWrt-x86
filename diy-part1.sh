306cb47e32259f6ca04b0f4a45630c8b516ba1f2
tengyuankoo <56631063+tengyuankoo@users.noreply.github.com>
2021-02-25 12:57:39 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -14,7 +14,7 @@ sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 # Add a feed source
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
 #git clone https://github.com/Lienol/openwrt-package.git package/luci-app-Lienol
-
+git clone https://github.com/xiaorouji/openwrt-passwall package/openwrt-passwall
 git clone https://github.com/jerrykuku/luci-app-jd-dailybonus.git
 git clone -b master https://github.com/vernesong/OpenClash.git package/luci-app-openclash
 git clone https://github.com/tty228/luci-app-serverchan.git package/luci-app-serverchan
